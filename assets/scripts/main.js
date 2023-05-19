77f2896c88397cf6eba69aa081b12da97add7f75
dflores1229 <d.flores1229@gmail.com>
2023-05-19 16:42:09 -0700

    completed lab 7

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,34 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ("serviceWorker" in navigator) {
+    // Supported!
+  
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
+    window.addEventListener('load', loadFunc);
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    // B3. TODO - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+    function loadFunc() {
+    navigator.serviceWorker.register("./sw.js").then(
+      (registration) => {
+        console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+        console.error(`Service worker registration failed: ${error}`);
+      }
+    );
+    console.log()
+    }
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  } else {
+    console.error("Service workers are not supported.");
+  }
 }
 
 /**
@@ -68,16 +87,46 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  //const items = JSON.stringify(localStorage.getItem('recipes'));
+  
+  if (localStorage.getItem('recipes') != null) {
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
+
+  
+  
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+  const myPromise = new Promise(promiseFunc);
+   
+  async function promiseFunc(resolve, reject) {
+    for (let i = 0; i < RECIPE_URLS.length; i++) {
+      try { 
+        const newRecipe = await fetch(RECIPE_URLS[i]);
+        const recipeJson = await newRecipe.json();
+        recipes.push(recipeJson);
+        if (i == RECIPE_URLS.length-1) {
+          saveRecipesToStorage(recipes);
+          const accept = resolve(recipes);
+        }  
+      } catch(error){
+        console.log(error);
+        const reject = reject(error);
+      }
+    }
+  };
+  
+
+  return myPromise;
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
--- sw.js
+++ sw.js
@@ -9,7 +9,15 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      const RECIPE_URLS = [
+        'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+        'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+        'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+        'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+      ];
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -22,7 +30,7 @@ self.addEventListener('activate', function (event) {
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
   // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
+  // subsequent network request URL and adding it manually would, be very taxing.
   // We will be adding all of the resources not specified in the intiial cache
   // list to the cache as they come in.
   /*******************************/
@@ -34,7 +42,33 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
+      });
+    });
+  }));
+});
+  /*
+    // Open the cache
+    event.respondWith(
+      fetch(event.request).then((fetchedResponse) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          caches.put(event.request, fetchedResponse.clone());
+        });
+
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request);
+      })
+    )
+  });*/
+  
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-});
+  
+
