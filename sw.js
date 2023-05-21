79f472dd21d90530180db9f4653aa6c2ca1a858a
dflores1229 <d.flores1229@gmail.com>
2023-05-20 22:11:49 -0700

    revised sw

--- sw.js
+++ sw.js
@@ -42,31 +42,20 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  /*
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        cache.put(event.request, fetchedResponse.clone());
-        return fetchedResponse;
-      });
+  
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        return (
+          cachedResponse || fetch(event.request).then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
+              return fetchedResponse;
+          })
+        );
     });
   }));
-});*/
-  
-    // Open the cache
-    event.respondWith(
-      fetch(event.request).then((fetchedResponse) => {
-        caches.open(CACHE_NAME).then((cache) => {
-          caches.put(event.request, fetchedResponse.clone());
-        });
+});
 
-        return fetchedResponse;
-      }).catch(() => {
-        // If the network is unavailable, get
-        return cache.match(event.request);
-      })
-    )
-  });
   
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
